75267fb6df8ec4c0e158506d81641734661cefde
startcaft <51418996@qq.com>
2018-05-07 23:04:37 +0800

    使用 mobx，mobx-react 来完成一个todolist。（observer，computed，autorun）以及mobx-react的observer

---
+++ mobx-react-todolist-demo/src/TodoView.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+
+const TodoView = observer((props) => {
+    let {todo} = props;
+    return (
+        <li>
+            <input
+                type="checkbox"
+                checked={todo.finished}
+                onClick={todo.finish.bind(todo)}
+                />
+            {todo.content}
+        </li>
+    )
+});
+
+
+export default TodoView;
